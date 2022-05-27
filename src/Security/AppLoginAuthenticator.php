ccbb8afcb50af98aa43167624f981d681c9063fe
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2022-05-27 18:17:43 +0200

    Create profile page

--- src/Security/AppLoginAuthenticator.php
+++ src/Security/AppLoginAuthenticator.php
@@ -77,9 +77,7 @@ class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_profile'));
     }
 
     protected function getLoginUrl(Request $request): string
